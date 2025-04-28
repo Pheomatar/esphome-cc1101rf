40d8bb9e6d9048827824b5598ee49fb254b52cb2
Pheomatar <163772796+Pheomatar@users.noreply.github.com>
2025-04-28 22:07:59 +0200

    Update cc1101rf.cpp

--- cc1101rf/cc1101rf.cpp
+++ cc1101rf/cc1101rf.cpp
@@ -1,1 +1,8 @@
+#include "cc1101rf.h"
+namespace esphome {
+namespace cc1101rf {
 
+// No additional implementation needed for this simple example
+
+}  // namespace cc1101rf
+}  // namespace esphome
